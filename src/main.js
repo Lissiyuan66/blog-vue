02fefe6cd7072f9d6ed305505f040c5c1766e975
Lissiyuan66 <464971640@qq.com>
2019-10-12 00:23:20 +0800

    blog vue 前后端分离

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import semantic from 'semantic'
+import '../node_modules/semantic-ui-css/semantic.min.css'
+// 设置反向代理，前端请求默认发送到 http://localhost:8443/api
+var axios = require('axios')
+axios.defaults.baseURL = 'http://localhost:8443/api'
+// 全局注册，之后可在其他组件中通过 this.$axios 发送数据
+Vue.prototype.$axios = axios
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
+
